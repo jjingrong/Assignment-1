62eea74de50fac4ef19e78b98c3cbc409376eff9
Lim Jing Rong <jing_rong92@hotmail.com>
2017-02-12 17:05:31 +0800

    mostly done, except exec

--- shell.c
+++ shell.c
@@ -1,19 +1,39 @@
+/*
+    Name: Lim Jing Rong
+    Matric: A0114395E
+ */
+
 #include <stdio.h>
 #include <unistd.h>
-#include <dirent.h>
-#define CHUNK 255
+#include <stdlib.h>
+#define MAX_LENGTH 255
 
 int main() {
+  // Function to run command
   int system(const char *command);
+  // Variable to read input
+  char input[MAX_LENGTH];
   
-  char input[CHUNK];
+  // Infinite loop to await command
   while(1) {
+    // Read in command
     printf(">");
-    scanf("%s", input);
-    printf("Input: %s",input);
-    printf("\n");
+    fgets(input, MAX_LENGTH, stdin);
     
-    // Execute command
-    system(input);
+    // Process management
+    int pid;
+    if ((pid = fork()) > 0 ) {
+      // Create a new process, printing out the new process’s pid.
+      printf("Loading new process with id %d\n", pid);
+    } else if (pid == 0) {
+      // In the child, print the pid of the parent, and load and execute the command entered by the user.
+      printf("Parent pid: %d\n", getppid());
+      // Execute command
+      system(input);
+      printf("\n");
+    } else {
+    // Print an error message if the command cannot be found or some other problem occurs.
+      perror("fork failed");
+    }
   }
 }
